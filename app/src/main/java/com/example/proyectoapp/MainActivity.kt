a5b39c24d41a3fac7b0018e3ebad95626bde56f4
Carmelo908 <carmelopepigauna@gmail.com>
2025-10-14 17:30:07 -0300

    Added UI login

--- app/src/main/java/com/example/proyectoapp/MainActivity.kt
+++ app/src/main/java/com/example/proyectoapp/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.proyectoapp.ui.theme.ProyectoAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +36,6 @@ class MainActivity : ComponentActivity() {
             ProyectoAPPTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +45,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Greeting(modifier: Modifier = Modifier) {
+    var name: String by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.logo_escuela),
+            contentDescription = "Logo de la Escuela Técnica N°2"
+        )
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Email") },
+            placeholder = { Text("") },
+            modifier = Modifier.padding(20.dp)
+        )
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Contraseña") },
+            placeholder = { Text("") },
+            modifier = Modifier.padding(20.dp)
+        )
+        Button(onClick = {  }, content = { Text("Enviar") })
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ProyectoAPPTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
