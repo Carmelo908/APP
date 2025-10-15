63487e986f8d5f5514100d8aeab1926aad12222a
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2025-10-15 09:57:36 -0300

    Add: primary title in login component

--- app/src/main/java/com/example/proyectoapp/MainActivity.kt
+++ app/src/main/java/com/example/proyectoapp/MainActivity.kt
@@ -22,10 +22,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import com.example.proyectoapp.ui.theme.ProyectoAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +46,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    var name: String by remember { mutableStateOf("") }
+    var email: String by remember { mutableStateOf("") }
+    var password: String by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -54,34 +56,31 @@ fun Greeting(modifier: Modifier = Modifier) {
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    )
-    {
+    ) {
+        Text(
+            text = "Asistente para docentes",
+            color = Color.Black,
+            modifier = Modifier.padding(bottom = 30.dp),
+            fontSize = 3.em
+        )
         Image(
             painter = painterResource(id = R.drawable.logo_escuela),
             contentDescription = "Logo de la Escuela Técnica N°2"
         )
         TextField(
-            value = name,
-            onValueChange = { name = it },
+            value = email,
+            onValueChange = { email = it },
             label = { Text("Email") },
-            placeholder = { Text("") },
+            placeholder = { Text("Correo electronico") },
             modifier = Modifier.padding(20.dp)
         )
         TextField(
-            value = name,
-            onValueChange = { name = it },
+            value = password,
+            onValueChange = { password = it },
             label = { Text("Contraseña") },
-            placeholder = { Text("") },
+            placeholder = { Text("Contraseña") },
             modifier = Modifier.padding(20.dp)
         )
-        Button(onClick = {  }, content = { Text("Enviar") })
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProyectoAPPTheme {
-        Greeting()
+        Button(onClick = { }, content = { Text("Iniciar sesion") })
     }
 }
